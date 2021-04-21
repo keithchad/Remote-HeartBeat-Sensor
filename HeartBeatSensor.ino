790b4981fbcfedb8e7c1275d4f67b90e3992bf4e
Chad <keithchad10@outlook.com>
2021-04-21 13:34:12 +0300

    adding libraries

--- HeartBeatSensor.ino
+++ HeartBeatSensor.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+//Created by Keith Chad on April 21st 2021
 
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+#define BLYNK_PRINT Serial
+#include <Blynk.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+ 
+#include "Wire.h"
+#include "Adafruit_GFX.h"
+#include "OakOLED.h"
+ 
+#define REPORTING_PERIOD_MS 1000
+
+void setup()
+{
+   
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+void loop()
+{
+    
 }
